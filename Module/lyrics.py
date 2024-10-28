1dc81cb40eeccf2e0672f6e8256a13f1efa89cc9
ArisDEV <tranthanhnam.ccc@gmail.com>
2024-10-28 14:12:43 +0700

    add cache function / fix when type of the lyric is timed

--- Module/lyrics.py
+++ Module/lyrics.py
@@ -3,9 +3,13 @@ from disnake import ApplicationCommandInteraction, Embed
 from utils.ClientUser import ClientUser
 from musicCore.player import MusicPlayer
 from musicCore.check import check_voice, has_player
+from utils.conv import time_format
+from musicCore.lyric_cache import LyricCache
+
 class Lyrics(commands.Cog):
     def __init__(self, bot):
         self.bot: ClientUser = bot
+        self.lyric_cache: LyricCache = LyricCache()
 
     @commands.slash_command(name="lyrics")
     async def lyrics(self, inter: ApplicationCommandInteraction):
@@ -25,20 +29,32 @@ class Lyrics(commands.Cog):
             return
 
         await inter.edit_original_response(embed=Embed(description="### <a:searching:1298557100431245354> Đang tìm lời bài hát..."))
-        try:
-            lyrics = await player.get_lyric(inter.guild.id)
-        except Exception:
-            lyrics = None
+
+        lyrics = self.lyric_cache.get_lyric(player.current.title)
+        if lyrics is None:
+            try:
+                lyrics = await player.get_lyric(inter.guild.id)
+                self.lyric_cache.add_lyric(player.current.title, lyrics)
+            except Exception:
+                lyrics = None
 
         embed = Embed()
         embed.title = f"Lời bài hát cho: {player.current.title}"
         embed.url = player.current.uri
         if lyrics is not None:
-            embed.description = lyrics["text"]
+            if lyrics["type"] == "text":
+                embed.description = lyrics["text"]
+                if lyrics["source"]:
+                    embed.set_footer(text=f"Nguồn: {lyrics['source'][8:]}")
+            if lyrics["type"] == "timed":
+                lyric = []
+                for lines in lyrics['lines']:
+                    lyric.append(f"[{time_format(lines['range']['start'])}] {lines['line'].replace('♪',' 🎵 🎶 🎵 🎶 🎵 🎶 ')}")
+                embed.description = "\n".join(lyric)
+                if lyrics["source"]:
+                    embed.set_footer(text=f"Nguồn: {lyrics['source']}")
             embed.colour = 0x07c1f0
             embed.set_thumbnail(player.current.artwork_url)
-            if lyrics["source"]:
-                embed.set_footer(text=f"Nguồn: {lyrics['source'][8:]}")
         else:
             embed.description = "```Không có hoặc nguồn phát nhạc không hỗ trợ```"
             embed.colour = 0xf01707
